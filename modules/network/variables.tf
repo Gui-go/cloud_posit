c0e190f8da398f5214f40588fa50ec9ca23b0376
Guilherme Viegas <46000780+Gui-go@users.noreply.github.com>
2024-11-24 07:57:17 +0100

    Create variables.tf

---
+++ modules/network/variables.tf
@@ -0,0 +1,19 @@
+variable "proj_name" {
+  description = "Project name"
+  type        = string
+}
+
+variable "proj_id" {
+  description = "Project identifier"
+  type        = string
+}
+
+variable "location" {
+  description = "Location of the resources"
+  type        = string
+}
+
+variable "tag_owner" {
+  description = "Tag to describe the owner of the resources"
+  type        = string
+}
