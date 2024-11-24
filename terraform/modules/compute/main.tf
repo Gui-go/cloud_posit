ce2e964792c372afbe52ecd6ca34f862ebc576c4
Guilherme Viegas <46000780+Gui-go@users.noreply.github.com>
2024-11-24 23:20:27 +0100

    Update main.tf

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -24,10 +24,19 @@ resource "google_compute_instance" "tf_computeinstance" {
     "user-data" = <<-EOF
       #!/bin/bash
 
-      echo "Personal RStudio repo -----------------------------------------------"
+      sudo apt update -y
+      sudo apt install -y git
+      
+      sudo apt install tree -y 
+      sudo apt install -y docker.io
+      sudo systemctl start docker
+      sudo systemctl enable docker
+      sudo curl -L "https://github.com/docker/compose/releases/download/v2.1.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+      sudo chmod +x /usr/local/bin/docker-compose
+
       git clone https://github.com/Gui-go/cloud_posit.git /home/guilhermeviegas1993/cloud_posit/
       sudo env R_PASS=passwd123 docker-compose -f /home/guilhermeviegas1993/cloud_posit/docker/docker-compose.yml up -d --build
-      sudo docker exec -t personal_rstudio bash -c 'chown -R rstudio:rstudio /home/rstudio/volume/'
+      sudo docker exec -t cloud_posit_container bash -c 'chown -R rstudio:rstudio /home/rstudio/volume/'
       sudo docker ps
 
       echo "VM init finished!"
